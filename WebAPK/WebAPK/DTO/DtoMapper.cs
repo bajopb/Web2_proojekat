d96bcd0c035f54652cb4cbaa2d2645dd33a341f9
bajopb <nikolla.vujovic@gmail.com>
2023-08-03 16:42:15 +0200

    backend...

---
+++ WebAPK/WebAPK/DTO/DtoMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using WebAPK.Models;
+
+namespace WebAPK.DTO
+{
+    public class DtoMapper:Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<User, RegisterDTO>().ReverseMap();
+            CreateMap<User, UserDTO>().ReverseMap();
+            CreateMap<User, ProfileEditDTO>().ReverseMap();
+            CreateMap<User, LoginDTO>().ReverseMap();
+            CreateMap<Order, OrderDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
--- WebAPK/WebAPK/Program.cs
+++ WebAPK/WebAPK/Program.cs
@@ -1,5 +1,14 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using WebAPK.Configuration;
+using WebAPK.DTO;
+using WebAPK.Interfaces;
+using WebAPK.Services;
+using Microsoft.AspNetCore.Authentication.Google;
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<Web2DbContext>(opt =>
@@ -9,7 +18,81 @@ builder.Services.AddDbContext<Web2DbContext>(opt =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c => {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPK", Version = "v1" });
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = "Enter the token",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            BearerFormat="JWT",
+            Scheme = "Bearer"
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                      }
+                    });
+    }
+    );
+builder.Services.AddScoped<DbContext, Web2DbContext>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IProfileService, ProfileSerivice>();
+builder.Services.AddScoped<IAdminService, AdminService>();
+builder.Services.AddScoped<IBuyerService, BuyerService>();
+builder.Services.AddScoped<ISellerSerivice, SellerService>();
+builder.Services.AddAutoMapper(typeof(DtoMapper));
+
+builder.Services.AddCors( o =>
+{
+    o.AddPolicy("All", p =>
+    {
+        p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey
+        (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+}).AddGoogle(o =>
+{
+    o.ClientId = builder.Configuration["Google:ClientID"];
+    o.ClientSecret = builder.Configuration["Google:ClientSecret"];
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -20,7 +103,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("All");
+
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
